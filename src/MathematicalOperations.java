676d9ddbe6228842376488ce29e304e2019224c4
Madhuri Tayade <codekul.madhuri@gmail.com>
2022-06-08 16:05:14 +0530

    mathematical operations

---
+++ src/MathematicalOperations.java
@@ -0,0 +1,16 @@
+public class MathematicalOperations {
+    public static void main(String[] args) {
+        int x = 52;
+        int y = 85;
+        int result;
+        //Addition
+        result = x + y;
+        System.out.println("Addition is " + result);
+        result = y - x;
+        System.out.println("Subtraction is " + result);
+        result = x * y;
+        System.out.println("Multiplication is " + result);
+        result = y / x;
+        System.out.println("Division is " + result);
+    }
+}
