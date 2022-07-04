2e91908cd4cb18016e88335df82836b48fd79f3e
Madhuri Tayade <codekul.madhuri@gmail.com>
2022-07-04 15:59:32 +0530

    Madhuri - interface intro

---
+++ src/interfacedemo/Bank.java
@@ -0,0 +1,30 @@
+package interfacedemo;
+
+public interface Bank {
+    double getRateOfInterest();
+    float N = 10;
+}
+interface BankOperations extends Bank{
+    double calculateInterest();
+}
+class HDFC implements BankOperations{
+
+    @Override
+    public double getRateOfInterest() {
+        return 10;
+    }
+
+    @Override
+    public double calculateInterest() {
+        return 0;
+    }
+}
+
+
+class BankImpl{
+    public static void main(String[] args) {
+        Bank bank;//reference variable
+        bank = new HDFC();
+        //bank = new ICICI();
+    }
+}
